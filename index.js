96946b8128d75c918ed799a9bbbe8bce8cece84e
BoHong Li <a60814billy@gmail.com>
2017-03-22 15:25:28 +0800

    feat: Implement auto migartion
    Reference Umzug, sequelize-cli. When app start, do migration automatically.

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 'use strict'
 
-const App = require('./lib')
+const path = require('path')
+const util = require('util')
 
-let app = new App()
-app.start()
+const logger = require('./lib/core/utils/logger')
+
+const env = process.NODE_ENV || 'development'
+const config = require('./lib/config/config.json')[env]
+
+function runApp () {
+  const App = require('./lib')
+
+  let app = new App()
+  app.start()
+}
+
+function autoMigration () {
+  const migrationUtil = require('./lib/core/migration')
+  const migrationFilePath = path.resolve(__dirname, './lib/model/migrations')
+
+  logger.info('Starting database auto migration')
+  logger.debug(`Migration Path: ${migrationFilePath}`)
+
+  migrationUtil.getSequelize(config.db).then(function (sequelize) {
+    sequelize.authenticate()
+      .then(function () {
+        logger.debug('Database connected.')
+      })
+      .then(function () {
+        return migrationUtil.createMigrator(sequelize)
+      })
+      .then(function (migrator) {
+        return migrationUtil.runMigration(sequelize, migrator)
+      })
+      .then(function () {
+        runApp()
+      })
+      .catch(function (err) {
+        if (err.message === 'SequelizeMeta schema not esixt.') {
+          logger.info('SequlizeMeta not found.')
+          return runApp()
+        }
+        throw err
+      })
+  })
+}
+
+if (util.isUndefined(config)) {
+  throw new Error('Fail to load config file')
+}
+
+if (config.db.auto_migration) {
+  autoMigration()
+} else {
+  runApp()
+}
